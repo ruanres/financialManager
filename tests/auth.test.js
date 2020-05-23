fd65f8cdfb4f448426ccda6b0eeb61102e675b9d
ruanres <ruan.silveira@ccc.ufcg.edu.br>
2020-05-22 21:44:01 -0300

    add signup route

--- tests/auth.test.js
+++ tests/auth.test.js
@@ -3,7 +3,9 @@ const app = require('../src/app');
 const { getEmail } = require('./utils');
 
 describe('Auth tests', () => {
-  const ROUTE = '/auth/signin';
+  const ROUTE = '/auth';
+  const SIGNIN_ROUTE = `${ROUTE}/signin`;
+  const SIGNUP_ROUTE = `${ROUTE}/signup`;
   let user;
 
   beforeEach(async () => {
@@ -11,16 +13,25 @@ describe('Auth tests', () => {
     await app.services.user.save(user);
   });
 
+  it('should create a user by signup', async () => {
+    const newUser = { name: 'tom', email: getEmail(), password: 'password' };
+    const response = await request(app).post(SIGNUP_ROUTE).send(newUser);
+    expect(response.status).toBe(201);
+    expect(response.body).not.toHaveProperty('password');
+    expect(response.body.name).toBe(newUser.name);
+    expect(response.body.email).toBe(newUser.email);
+  });
+
   it('should return token on login', async () => {
     const credentials = { email: user.email, password: user.password };
-    const response = await request(app).post(ROUTE).send(credentials);
+    const response = await request(app).post(SIGNIN_ROUTE).send(credentials);
     expect(response.status).toBe(200);
     expect(response.body.token).toBeDefined();
   });
 
   it('should not authenticate the user when the password is wrong', async () => {
     const credentials = { email: user.email, password: 'wrong password' };
-    const response = await request(app).post(ROUTE).send(credentials);
+    const response = await request(app).post(SIGNIN_ROUTE).send(credentials);
     expect(response.status).toBe(401);
     expect(response.body.token).toBeUndefined();
     expect(response.body.error).toBe('Wrong password');
@@ -28,7 +39,7 @@ describe('Auth tests', () => {
 
   it('should not authenticate if the user does not exist', async () => {
     const credentials = { email: 'notFound@mail.com', password: 'password' };
-    const response = await request(app).post(ROUTE).send(credentials);
+    const response = await request(app).post(SIGNIN_ROUTE).send(credentials);
     expect(response.status).toBe(404);
     expect(response.body.token).toBeUndefined();
     expect(response.body.error).toBe('User not found');
