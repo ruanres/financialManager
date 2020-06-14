b433e037e6f3f11fa0ec13522a706bcf492f228d
ruanres <ruan.silveira@ccc.ufcg.edu.br>
2020-06-14 00:49:14 -0300

    fixed account tests

--- tests/account.test.js
+++ tests/account.test.js
@@ -1,26 +1,23 @@
-const { makeRequest, clearDB, getEmail } = require('./utils');
+const { makeRequest, getTestEntities } = require('./utils');
 const app = require('../src/app');
 
 describe('Accounts tests', () => {
   const MAIN_ROUTE = '/accounts';
-  let user;
-  let otherUser;
+  const { users, accounts } = getTestEntities();
+  const [user1] = users;
+  const [userAcc,, otherUserAcc] = accounts;
   let token;
 
   beforeAll(async () => {
-    await clearDB();
-    const userData = { name: 'userA', email: `user.${getEmail()}`, password: 'password' };
-    const otherUserData = { name: 'userB', email: `other.${getEmail()}`, password: 'password' };
-    [user] = await app.services.user.save(userData);
-    [otherUser] = await app.services.user.save(otherUserData);
-
     const response = await app.services.auth.signin({
-      email: userData.email, password: userData.password,
+      email: user1.email, password: user1.password,
     });
     token = response.token;
   });
 
-  afterAll(clearDB);
+  beforeEach(async () => {
+    await app.db.seed.run();
+  });
 
   it('should create an account', async () => {
     const newAccount = { name: 'acc 1' };
@@ -39,65 +36,48 @@ describe('Accounts tests', () => {
   });
 
   it('should list all the logged user accounts', async () => {
-    await await app.db('accounts').del();
-    const accountData = { name: 'acc 1', user_id: user.id };
-    const otherAccountData = { name: 'acc 2', user_id: otherUser.id };
-    const [userAccount] = await app.db('accounts').insert(
-      [accountData, otherAccountData],
-      ['id'],
-    );
     const result = await makeRequest('get', MAIN_ROUTE, token);
     expect(result.status).toBe(200);
-    expect(result.body.length).toBe(1);
-    expect(result.body[0].id).toBe(userAccount.id);
+    expect(result.body.length).toBe(2);
+    expect(result.body[0].id).toBe(user1.id);
   });
 
   it('should not access another users accounts', async () => {
-    const account = { name: 'acc from other user', user_id: otherUser.id };
-    const [newAccount] = await app.db('accounts').insert(account, ['id']);
-    const result = await makeRequest('get', `${MAIN_ROUTE}/${newAccount.id}`, token);
+    const result = await makeRequest('get', `${MAIN_ROUTE}/${otherUserAcc.id}`, token);
     expect(result.status).toBe(403);
     expect(result.body.error).toBe('User is not allowed access this resource');
   });
 
   it('should return an account per id', async () => {
-    const account = { name: 'acc 1', user_id: user.id };
-    const [newAccount] = await app.db('accounts').insert(account, ['id']);
-    const result = await makeRequest('get', `${MAIN_ROUTE}/${newAccount.id}`, token);
+    const result = await makeRequest('get', `${MAIN_ROUTE}/${userAcc.id}`, token);
     expect(result.status).toBe(200);
-    expect(result.body.name).toBe(account.name);
-    expect(result.body.user_id).toBe(account.user_id);
+    expect(result.body.name).toBe(userAcc.name);
+    expect(result.body.user_id).toBe(userAcc.user_id);
   });
 
   it('should update an account', async () => {
-    const account = { name: 'acc 1', user_id: user.id };
-    const [newAccount] = await app.db('accounts').insert(account, ['id']);
     const newName = 'updated name';
-    const result = await makeRequest('put', `${MAIN_ROUTE}/${newAccount.id}`, token, { name: newName });
+    const result = await makeRequest('put', `${MAIN_ROUTE}/${userAcc.id}`, token, { name: newName });
     expect(result.status).toBe(200);
     expect(result.body.name).toBe(newName);
-    expect(result.body.user_id).toBe(account.user_id);
+    expect(result.body.user_id).toBe(userAcc.user_id);
   });
 
   it('should not update another user account', async () => {
-    const account = { name: 'acc 1', user_id: otherUser.id };
-    const [newAccount] = await app.db('accounts').insert(account, ['id']);
-    const result = await makeRequest('put', `${MAIN_ROUTE}/${newAccount.id}`, token, { name: 'updated name' });
+    const result = await makeRequest('put', `${MAIN_ROUTE}/${otherUserAcc.id}`, token, { name: 'updated name' });
     expect(result.status).toBe(403);
     expect(result.body.error).toBe('User is not allowed access this resource');
   });
 
   it('should delete an account', async () => {
-    const account = { name: 'acc 1', user_id: user.id };
+    const account = { name: 'acc 1', user_id: user1.id };
     const [newAccount] = await app.db('accounts').insert(account, ['id']);
     const result = await makeRequest('delete', `${MAIN_ROUTE}/${newAccount.id}`, token);
     expect(result.status).toBe(204);
   });
 
   it('should not delete another user account', async () => {
-    const account = { name: 'acc 1', user_id: otherUser.id };
-    const [newAccount] = await app.db('accounts').insert(account, ['id']);
-    const result = await makeRequest('delete', `${MAIN_ROUTE}/${newAccount.id}`, token);
+    const result = await makeRequest('delete', `${MAIN_ROUTE}/${otherUserAcc.id}`, token);
     expect(result.status).toBe(403);
     expect(result.body.error).toBe('User is not allowed access this resource');
   });
@@ -110,13 +90,7 @@ describe('Accounts tests', () => {
   });
 
   it('should not delete an account with associated transactions', async () => {
-    const account = { name: 'acc 1', user_id: user.id };
-    const [newAccount] = await app.db('accounts').insert(account, ['id']);
-    const transaction = {
-      description: 'T1', type: 'I', ammount: 100, acc_id: newAccount.id, date: new Date(),
-    };
-    await app.db('transactions').insert(transaction, ['id']);
-    const result = await makeRequest('delete', `${MAIN_ROUTE}/${newAccount.id}`, token);
+    const result = await makeRequest('delete', `${MAIN_ROUTE}/${userAcc.id}`, token);
     expect(result.status).toBe(400);
     expect(result.body.error).toBe('This account has transactions related to it');
   });
